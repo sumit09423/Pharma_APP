d5be20dcf39f20da165a827b6f0636cbc7072c5e
aryanpatel1503 <patelaryankumar1503@gmail.com>
2024-05-14 20:53:07 +0530

    login page added

--- index.js
+++ index.js
@@ -5,5 +5,25 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  // Specify custom property
+  myOwnProperty: true,
+  // Specify custom property in nested object
+  colors: {
+    ...DefaultTheme.colors,
+    themeColor: '#0EC5C1',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
